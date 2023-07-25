99dc0387de9f4ee6a72102fec86ba34a741f2d41
bardoor <93031169+bardoor@users.noreply.github.com>
2023-07-25 17:09:34 +0300

    Расчленил класс на функции

--- model.py
+++ model.py
@@ -1,64 +1,53 @@
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-
-
+from keras.callbacks import ModelCheckpoint
+import numpy as np
 import dataset
 
+def create_model(classes_count, dropout=0.2, base_units=16):
+    if base_units < 4:
+        raise Exception('base_units must be greater than 4')
+    
+    model = keras.Sequential([
+        layers.Input(shape=(None, 34)),
+        layers.LSTM(units=base_units, return_sequences=True),
+        layers.LSTM(units=int(base_units/2)),
+        layers.Dense(units=base_units),
+        layers.Dropout(dropout),
+        layers.Dense(units=int(base_units/2)),
+        layers.Dropout(dropout),
+        layers.Dense(units=classes_count, activation='softmax') 
+    ])
 
-class ActionEstimationModel:
-
-    def __init__(self, weights=None, train_dataset=None):
-        self.model = keras.Sequential([
-            layers.LSTM(units=16, input_shape=(None, 34), return_sequences=True),
-            layers.LSTM(units=8),
-            layers.Dense(units=16),
-            layers.Dropout(0.2),
-            layers.Dense(units=8),
-            layers.Dropout(0.2),
-            layers.Dense(units=dataset.ACTIVITY_CLASSES_NUMBER, activation='softmax')
-        ])
-
-        self.model.compile(
-            loss='categorical_crossentropy',
+    model.compile(
+        loss='categorical_crossentropy',
             optimizer='adam',
             metrics=['accuracy']
-        )
+    )
 
-        if weights is not None:
-            self.load_weights(weights)
-        
-        if train_dataset is not None:
-            self.history = self.train(train_dataset)
+    return model
 
-    def load_weights(self, weights):
-        self.model.load_weights(weights)
+def train_model(model, dataset_file, output_weights="best_model.h5"):
+    (x_train, y_train), (x_test, y_test) = dataset.load_data(dataset_file)
 
-    def train(self, dataset_file, output_weights="best_model.h5"):
-        (x_train, y_train), (x_test, y_test) = dataset.load_data(dataset_file)
+    callbacks = [
+        ModelCheckpoint(filepath=output_weights, monitor='val_loss', save_best_only=True)
+    ]
 
-        callbacks = [
-            # EarlyStopping(monitor='val_loss', patience=10),
-            ModelCheckpoint(filepath=output_weights, monitor='val_loss', save_best_only=True)
-        ]
+    history = model.fit(
+        x_train, y_train,
+        epochs=250,
+        batch_size=32,
+        validation_split=0.2,
+        shuffle=False,
+        callbacks=callbacks
+    )
 
-        history = self.model.fit(
-            Xs, ys,
-            epochs=250,
-            batch_size=32,
-            validation_split=0.2,
-            shuffle=False,
-            callbacks=callbacks
-        )
+    return history
+    
+def predict(model, path_to_video, deviation_threshold=0.1):
+    for predicion in dataset.generate(path_to_video):
+        predicted_class = np.std(predicion) > deviation_threshold if tf.argmax(model(predicion)[0]) else None
+        yield predicted_class
 
-        return history
-
-    def predict(self, path_to_video):
-        predicted_labels = []
-
-        for keypoints_seq in dataset.generate(path_to_video):
-            predicted_class = tf.argmax(self.model(keypoints_seq)[0])
-            predicted_labels.append(dataset.ACTIVITY_LABELS[predicted_class])
-        
-        return predicted_labels
